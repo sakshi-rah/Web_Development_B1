5968356007484c759ef6c99811405952c589b605
Sakshi Rahangdale <rahangdalesakshi2@gmail.com>
2026-04-02 21:34:03 +0530

    Day-32 (Shopping Card Logic Project)

---
+++ Day-32(Shopping Card )/README.md
@@ -0,0 +1,15 @@
+
+## 📌 NOTES (IMPORTANT)
+
+ 1. Array → used to store multiple items
+ 2. Object → used to store item details (name, price, qty)
+ 3. find() → check existing item
+ 4. forEach() → loop through items
+ 5. DOM → update HTML using JS
+ 6. innerHTML → insert dynamic content
+ 7. Events (onclick) → handle button clicks
+ 8. Template literals → easy dynamic HTML
+ 9. Functions → reusable code
+ 10. splice() → remove item from array
+ 
+ 👉 This is a basic e-commerce cart logic used in real projects 
---
+++ Day-32(Shopping Card )/script.js
@@ -0,0 +1,61 @@
+// ==========================
+// 🛒 SIMPLE SHOPPING CART USING JS
+// ==========================
+
+// Array to store cart items
+let cart = [];
+
+// Add item to card
+function addItem(name, price){
+    //find()
+    let item = cart.find(i => i.name === name);
+
+    if(item){
+        item.qty++;
+    }else{
+        cart.push({name: name, price: price, qty: 1});
+    }
+
+    showItem();
+}
+
+//Display Cart Item
+function showItem(){
+    let list = document.getElementById("cart");
+
+    let total = 0;
+    list.innerHTML = "";
+
+    cart.forEach((item, index)=>{
+        total += item.price * item.qty;
+        list.innerHTML += `
+        <li>
+        ${item.name} (Rs. ${item.price} * ${item.qty})
+        
+        <button onclick = "inc(${index})"> + </button>
+        <button onclick = "dec(${index})"> - </button>
+        <button onclick = "dlt(${index})"> Remove </button>
+        </li>`
+    });
+
+    document.getElementById("total").innerText = "Total: Rs." + total;
+}
+
+function inc(i){
+    cart[i].qty++;
+    showItem();
+}
+
+function dec(i){
+    if(cart[i].qty>1){
+        cart[i].qty--;
+    }else{
+        cart.splice(i, 1);
+    }
+    showItem();
+}
+
+function dlt(i){
+    cart.splice(i, 1);
+    showItem();
+}
